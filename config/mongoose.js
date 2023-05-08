825ad8b2de654efbabbc21cef7a6adc12bc45719
Geoffrey <acgeoffrey@>
2023-05-08 18:52:48 +0530

    production

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/habit_tracker");
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
 const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
+db.on('error', console.error.bind(console, 'Error connecting to MongoDB'));
 
-db.once("open", function () {
-  console.log("Connected to database :: MongoDB");
+db.once('open', function () {
+  console.log('Connected to database :: MongoDB');
 });
 
 module.exports = db;
